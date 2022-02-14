210ab6e6a1cfea7248a044c28593d8ac2b1e5be7
Ahmet Bozaci <ahmt9417@gmail.com>
2022-02-14 18:38:57 +0300

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :greetings
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
 end
+
